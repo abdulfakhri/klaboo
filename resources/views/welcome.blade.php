40d1665d468764a456522de5c90ff5e4f0c23736
Abdul Fakhri <afakhri2022@hotmail.com>
2022-06-10 10:46:29 -0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -103,7 +103,6 @@ body, html {
    <div class="col-lg-6">
    <div class="w3-right">
 <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-
 <lottie-player src="https://assets7.lottiefiles.com/packages/lf20_jn6admcl.json" background="transparent"  speed="1"  style="width: 1000px; height: 1000px;" loop autoplay></lottie-player>
 </div>
 
