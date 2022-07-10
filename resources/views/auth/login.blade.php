1cf362d58867342f54f16e639723f874659adad9
Abdul Fakhri <afakhri2022@hotmail.com>
2022-07-09 22:54:27 -0800

    ui reactjs changed to vuejs

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.logs')
+@extends('layouts.app')
 
 @section('content')
 <div class="container">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,51 +7,30 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Klabo') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-blue-grey.css">
-<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body  class="w3-theme-l5">
+<body>
     <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-    <div class="w3-top">
- <div class="w3-bar w3-theme w3-left-align w3-large">
-  <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large " href="javascript:void(0);" onclick="openNav()"><i class="fa fa-bars"></i></a>
-  <a href="{{ url('/') }}" class="w3-bar-item w3-button w3-padding-large w3-theme-d4 w3-left"><i class="fa fa-home w3-margin-right"></i></a>
-  <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white  w3-left" title="Account Settings">Teams</a>
-  <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white  w3-left" title="Account Settings">Project</a>
-  <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white  w3-left" title="Account Settings">Reports</a>
-  <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white  w3-left" title="Account Settings">KDS</a>
-
-
-
-
-
-  <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white  w3-right" title="News"><i class="fa fa-certificate"></i></a>
-
-
-  <input type="text" name="search" placeholder="Search.." class="w3-bar-item w3-input w3-border w3-bar-item w3-button w3-hide-small w3-right w3-padding-large w3-hover-white" style="width:200px">
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
 
@@ -93,91 +72,12 @@
                         @endguest
                     </ul>
                 </div>
-
-  <div class="w3-dropdown-hover w3-hide-small w3-right">
-    <button class="w3-button w3-padding-large" title="Notifications"><i class="fa fa-user"></i></button>
-    <div class="w3-dropdown-content w3-card-4 w3-bar-block" style="width:300px">
-
-      <a href="#" class="w3-bar-item w3-button">User Profile</a>
-      <a href="#" class="w3-bar-item w3-button">Email</a>
-      <hr>
-      <div class="m-4">
-    <div class="btn-group">
-        <input type="checkbox" class="btn-check" name="options" id="check1" autocomplete="off" checked>
-        <label class="btn btn-outline-primary" for="check1">Online</label>
-
-        <input type="checkbox" class="btn-check" name="options" id="check2" autocomplete="off">
-        <label class="btn btn-outline-primary" for="check2">Busy </label>
-
-    </div>
-</div>
-      <a href="#" class="w3-bar-item w3-button">Personal & Settings</a>
-      <a href="#" class="w3-bar-item w3-button">Email Preferences</a>
-      <a href="#" class="w3-bar-item w3-button">Help</a>
-
-      <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-    </div>
-  </div>
-  <a href="#" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white  w3-right" title="Messages"><i class="material-icons">message</i></a>
-  <div class="w3-dropdown-hover w3-hide-small w3-right">
-    <button class="w3-button w3-padding-large" title="Notifications"><i class="fa fa-bell"></i><span class="w3-badge w3-right w3-small w3-green">3</span></button>
-    <div class="w3-dropdown-content w3-card-4 w3-bar-block" style="width:300px">
-      <a href="#" class="w3-bar-item w3-button">One new friend request</a>
-      <a href="#" class="w3-bar-item w3-button">John Doe posted on your wall</a>
-      <a href="#" class="w3-bar-item w3-button">Jane likes your post</a>
-    </div>
-  </div>
-
- </div>
-
-</div>
-
-<!-- Navbar on small screens -->
-<div id="navDemo" class="w3-bar-block w3-theme-d2 w3-hide w3-hide-large w3-hide-medium w3-large">
-  <a href="#" class="w3-bar-item w3-button w3-padding-large">Teams</a>
-  <a href="#" class="w3-bar-item w3-button w3-padding-large">Project</a>
-  <a href="#" class="w3-bar-item w3-button w3-padding-large">Reports</a>
-  <a href="#" class="w3-bar-item w3-button w3-padding-large">KDS</a>
-</div>
-
-<!-- Page Container -->
-
-
+            </div>
+        </nav>
 
         <main class="py-4">
             @yield('content')
         </main>
     </div>
 </body>
-<script>
-    function myFunction(id) {
-    var x = document.getElementById(id);
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-      x.previousElementSibling.className += " w3-theme-d1";
-    } else {
-      x.className = x.className.replace("w3-show", "");
-      x.previousElementSibling.className =
-      x.previousElementSibling.className.replace(" w3-theme-d1", "");
-    }
-  }
-
-  // Used to toggle the menu on smaller screens when clicking on the menu button
-  function openNav() {
-    var x = document.getElementById("navDemo");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else {
-      x.className = x.className.replace(" w3-show", "");
-    }
-  }
-</script>
 </html>
